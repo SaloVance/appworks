0cd256ad4f040c8110bb1ac62337341a7fe39659
Guolong Ding <dingguolong@baidu.com>
2014-06-12 17:05:30 +0800

    init commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.write('<script type="text/javascript" src="./js/jquery.js"></script>');
+document.write('<script type="text/javascript" src="./js/jquery.easing.1.3.js"></script>');
+document.write('<script type="text/javascript" src="./js/touch.js"></script>');
+document.write('<script type="text/javascript" src="./js/appworks.js"></script>');
+document.write('<script type="text/javascript" src="./js/doodle.js"></script>');
